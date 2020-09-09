83a75b58a0d4a00724fac0146e7aeb60c731023d
djeriko <jerikosalivan@gmail.com>
2020-09-09 17:53:17 +0300

    added post state

--- lib/bloc/post_bloc.dart
+++ lib/bloc/post_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_infinite_list/post.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
